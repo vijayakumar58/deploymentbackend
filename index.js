7a469facc318beae8999e23d999f12e32e3f51cf
vijayakumar58 <vijayalingam58@gmail.com>
2023-04-08 20:28:11 +0530

    add deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen(process.env.PORT || 3001);
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin : "http://localhost:3000"
+    origin : "https://marvelous-sprite-474609.netlify.app"
 }))
 
 app.get('/', function(req,res){
